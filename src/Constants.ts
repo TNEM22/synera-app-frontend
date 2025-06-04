799bb3108232c0d31b93f8737b08006c6ce3c91b
tnem <tanmay.e.mahale@gmail.com>
2025-06-04 16:39:46 +0530

    updated-url

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,2 +1,3 @@
 // export const SERVER_URL = "https://todo-backend-b2rj.onrender.com";
-export const SERVER_URL = "http://localhost:3000";
+// export const SERVER_URL = "http://localhost:3000";
+export const SERVER_URL = 'https://synera-app-backend.onrender.com';
